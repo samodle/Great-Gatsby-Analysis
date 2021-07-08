641edd4a8f54863f89407dcf71fee6796858ce1c
Sam Odle <sro27@cornell.edu>
2021-07-08 17:53:16 -0400

    save

--- longestWordsSentences.R
+++ longestWordsSentences.R
@@ -36,3 +36,42 @@ print(ch2TenLongestSentences)
 message("Chapter 3:")
 print(ch3TenLongestSentences)
 
+
+message("-")
+message("--- Use openNLP to mark the parts of speech for the 10 longest sentences found in part b for nouns and verbs having a length of 5 or greater ---")
+
+
+#for(i in ch1TenLongestSentences){
+  message("Sentence: ")
+  s <- as.String(ch1TenLongestSentences[1])
+  print(s)
+  
+  ## Need sentence and word token annotations.
+  sent_token_annotator <- openNLP::Maxent_Sent_Token_Annotator()
+  word_token_annotator <- openNLP::Maxent_Word_Token_Annotator()
+  a2 <- NLP::annotate(s, list(sent_token_annotator, word_token_annotator))
+  
+  pos_tag_annotator <- Maxent_POS_Tag_Annotator()
+  #pos_tag_annotator
+  a3 <- NLP::annotate(s, pos_tag_annotator, a2)
+  #a3
+  
+  ## Variant with POS tag probabilities as (additional) features.
+  head(NLP::annotate(s, Maxent_POS_Tag_Annotator(probs = TRUE), a2))
+  
+  ## Determine the distribution of POS tags for word tokens.
+  a3w <- subset(a3, type == "word")
+  tags <- sapply(a3w$features, `[[`, "POS")
+  message("Tags: ")
+  print(tags)
+  print(table(tags))
+  ## Extract token/POS pairs (all of them): easy.
+  message("-")
+  print(sprintf("%s/%s", s[a3w], tags))
+  
+  ## Extract pairs of word tokens and POS tags for second sentence:
+  message("Pairs of word tokens and POS tags: ")
+  a3ws2 <- annotations_in_spans(subset(a3, type == "word"),
+                                subset(a3, type == "sentence")[2L])[[1L]]
+  print(sprintf("%s/%s", s[a3ws2], sapply(a3ws2$features, `[[`, "POS")))
+#}
